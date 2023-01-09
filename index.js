efd8014fe7e2c12ca2ef5e2151a4630ad52b5f29
Kyle Conarro <conarro@users.noreply.github.com>
2023-01-09 11:14:10 -0500

    Adds allowed args for addressing Puppeteer font rendering issues
    More on the underlying issue: https://github.com/puppeteer/puppeteer/issues/2410

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ process.env.FUNCTIONS_EMULATOR = 'aws_lambda';
 process.env.AWS_EXECUTION_ENV = 'AWS_Lambda_nodejs12.x';
 
 const chromium = require('chrome-aws-lambda');
-
 let autocodePuppeteer = chromium.puppeteer;
 let puppeteerLaunch = chromium.puppeteer.launch.bind(autocodePuppeteer);
+
+const allowedArgs = ['--font-render-hinting=none', '--font-render-hinting=medium']
+
 autocodePuppeteer.launch = async (options) => {
   options = options || {};
   if (options.hasOwnProperty('args')) {
-    throw new Error('You may not specify a custom "args" option when launching Puppeteer in Autocode.')
+    options.args.forEach((arg) => {
+      if (!allowedArgs.includes(arg)) {
+        throw new Error(`You may not specify ${arg} as a custom "args" option when launching Puppeteer in Autocode. Allowed custom "args": ${allowedArgs}`)
+      }
+    })
   }
   if (options.hasOwnProperty('executablePath')) {
     throw new Error('You may not specify a custom "executablePath" option when launching Puppeteer in Autocode.')
@@ -22,7 +28,7 @@ autocodePuppeteer.launch = async (options) => {
   if (!options.hasOwnProperty('ignoreHTTPSErrors')) {
     options.ignoreHTTPSErrors = true;
   }
-  options.args = chromium.args;
+  options.args = Object.assign(chromium.args, options.args);
   options.executablePath = await chromium.executablePath;
   options.headless = chromium.headless;
   return puppeteerLaunch(options);
